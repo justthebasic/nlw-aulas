c00e4607c61fa14b153c4ead1ba424039b8930ea
Erick <98369791+justthebasic@users.noreply.github.com>
2023-06-09 13:01:47 -0300

    config deploy

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { PageHeader } from "../../components/PageHeader";
 
 
